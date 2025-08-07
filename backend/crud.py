37e5f1f6fb9796aa32646ebfc9bae7f630deb0f1
danilovieira37 <danilovieira37@icloud.com>
2025-08-06 21:12:04 -0300

    add crud completo

--- backend/crud.py
+++ backend/crud.py
@@ -2,13 +2,13 @@ from sqlalchemy.orm import Session
 from schemas import ProductUpdate, ProductCreate
 from models import ProductModel
 
+# get where id = 1
+def get_product(db: Session, product_id: int):
+    return db.query(ProductModel).filter(ProductModel.id == product_id).first()
+
 # get all (select * from)
 def get_products(db: Session):
     return db.query(ProductModel).all()
-
-# get where id = 1
-def get_products(db: Session, product_id: int):
-    return db.query(ProductModel).filter(ProductModel.id == product_id).first()
 
 # insert into (create)
 def create_product(db: Session, product: ProductCreate):
--- backend/router.py
+++ backend/router.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from database import SessionLocal, get_db
-from schema import ProductResponse, ProductUpdate, ProductCreate
+from schemas import ProductResponse, ProductUpdate, ProductCreate
 from typing import List
 from crud import (
     create_product
@@ -13,6 +13,11 @@ from crud import (
 
 router = APIRouter()
 
+### Criar minha rota de adicionar um item
+@router.post("/products/", response_model=ProductResponse)
+def create_product_route(product: ProductCreate, db: Session = Depends(get_db)):
+    return create_product(db=db, product=product)
+
 ### Criar minha rota de buscar todos os itens
 ### Sempre vamos ter dois atributos orbigatorios, o PATH e o meu RESPONSE.
 @router.get("/products", response_model=List[ProductResponse])
@@ -21,21 +26,16 @@ def read_all_products(db: Session = Depends(get_db)):
     return products
 
 ### Criar minha rota de buscar 1 item
-@router.get("/products/{product_id}", respose_model=ProductResponse)
+@router.get("/products/{product_id}", response_model=ProductResponse)
 def read_one_product(product_id: int, db: Session = Depends(get_db)):
     db_product = get_product(db=db, product_id=product_id)
     if db_product is None:
         raise HTTPException(status_code=404, detail="voce ta querendo buscar um produto que nao existe.")
     return db_product
 
-### Criar minha rota de adicionar um item
-@router.post("/products/", response_model=ProductResponse)
-def create_product(product: ProductCreate, db: Session = Depends(get_db)):
-    return create_product(product=product, db=db)
-
 ### Criar minha rota de deletar um item
 @router.delete("/products/{product_id}", response_model=ProductResponse)
-def delete_product(product_id: int, db: Session = Depends(get_db)):
+def delete_product_route(product_id: int, db: Session = Depends(get_db)):
     product_db = delete_product(product_id=product_id, db=db)
     if product_db is None:
         raise HTTPException(status_code=404, detail="voce ta querendo deletar um produto que nao existe.")
@@ -43,8 +43,10 @@ def delete_product(product_id: int, db: Session = Depends(get_db)):
 
 ### Criar minha rota de fazer update nos itens
 @router.put("/products/{product_id}", response_model=ProductResponse)
-def atualizar_product(product_id: int, product: ProductUpdate, db: Session = Depends(get_db)):
-    product_db = update_product(db=db, product_id=product_id, product=product)
-    if product_db is None:
-        HTTPException(status_code=404, detail="voce ta querendo atualizar um produto que nao existe.")
-    product_db
+def update_product_route(
+    product_id: int, product: ProductUpdate, db: Session = Depends(get_db)
+):
+    db_product = update_product(db, product_id=product_id, product=product)
+    if db_product is None:
+        raise HTTPException(status_code=404, detail="Product not found")
+    return db_product
